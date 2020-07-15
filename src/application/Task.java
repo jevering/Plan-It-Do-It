2e763fc284c0399f48249abfcf0947cd0937c112
Jerry Everingham <67870815+jevering@users.noreply.github.com>
2020-07-15 09:23:53 -0700

    Updated Task file

--- src/application/Task.java
+++ src/application/Task.java
@@ -1,8 +1,22 @@
 package application;
 
-public class Task {
+import java.util.Date;
 
-	public Task() {
+public class Task {
+	private static final int LOW = 1;
+	private static final int MEDIUM = 2;
+	private static final int HIGH = 3;
+	
+	private String name;
+	private StringBuffer details = new StringBuffer(25);
+	private int priority = MEDIUM;
+	private Date deadline;
+	private Date created;
+	private int repeatPeriod = -1;
+	private String category;
+
+	public Task(String name) {
+		this.name = name;
 		// TODO Auto-generated constructor stub
 	}
 
