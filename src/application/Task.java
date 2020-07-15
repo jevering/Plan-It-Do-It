2e637fed34892aec75c9c4b896da7a908245f328
Jerry Everingham <67870815+jevering@users.noreply.github.com>
2020-07-15 09:21:39 -0700

    Adding preliminary Task Class

---
+++ src/application/Task.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Task {
+
+	public Task() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getUrgency() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/application/TaskComparator.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Comparator;
+
+public class TaskComparator implements Comparator<Task> {
+
+	public TaskComparator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int compare(Task task1, Task task2) {
+		double priority1 = task1.getUrgency();
+		double priority2 = task2.getUrgency();
+		
+		if (priority1 > priority2) {
+			return 1;
+		} else if (priority1 < priority2) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+}
